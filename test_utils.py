ba446e61cb832cb002af805bd7dc79067386a820
Michael Koller <koller_michael@outlook.de>
2025-10-11 22:09:16 +0200

    update test_utils.py: add tests for compound interest functions

--- test_utils.py
+++ test_utils.py
@@ -1,17 +1,22 @@
-import utils
+import utils as ut
+import sys
 
 TOL = 1e-6  # tolerance
+
+INFLATIONS = [i / 100 for i in range(1, 5)]
+MONTHLY_CONTRIBUTIONS = [i * 100 for i in range(1, 31)]
+RATES = [i / 100 for i in range(10)]
+START_BALANCES = [i * 100_000 for i in range(5)]
+YEARS = list(range(1, 31))
 
 
 def test_annual_to_monthly():
     """
     Compare growing a value for twelve months to growing it for one year.
     """
-    for r in range(1, 10):
-        rate = r / 100
-        for i in range(4):
-            x = 123 * i
-            m_rate = utils.annual_to_monthly(rate)
+    for rate in RATES:
+        for x in START_BALANCES:
+            m_rate = ut.annual_to_monthly(rate)
             x1 = x  # grow per month
             for _ in range(12):
                 x1 *= m_rate
@@ -20,27 +25,41 @@ def test_annual_to_monthly():
             assert abs(x1 - x2) < TOL
 
 
-def test_end_balance():
+def test_compound_interest():
     """
-    Just a sanity check:
-    a) The end balance should be larger than the start balance.
-    b) End balance divided by invested months should be larger than the monthly
-       investment.
-    c) End balance should be larger than the total investment.
+    Compare the compound interest formula to a manual computation.
     """
-    for s in range(4):
-        sb = s * 1e6  # start balance
-        for m in range(1, 10):
-            mc = m * 100  # monthly contribution
-            for y in range(1, 30):  # years to grow
-                for r in range(1, 10):
-                    rate = r / 100  # annual rate of return
-                    eb = utils.end_balance(sb, mc, y, rate)
-                    ti = sb + mc * 12 * y  # total investment
+    for y in YEARS:
+        for rate in RATES:
+            for sb in START_BALANCES:
+                for mc in MONTHLY_CONTRIBUTIONS:  # regular contributions
+                    for cpy in [2, 4, 12]:  # contributions per year
+                        ci = ut.compound_interest(sb, rate, mc, cpy, y)
 
-                    assert eb > sb  # a)
-                    assert eb / 12 / y > mc  # b)
-                    assert eb > ti  # c)
+                        # compute the compount interest "manually"
+                        regular_rate = (1 + rate) ** (1 / cpy)
+                        m = sb
+                        for _ in range(y * cpy):
+                            m *= regular_rate
+                            m += mc
+
+                        assert abs(ci - m) < TOL
+
+
+def test_compound_interest_rate():
+    """
+    Compute a compound interest final balance and use compound_interest_rate to get the
+    rate that was used. Compare the true rate to the computed rate.
+    """
+    for y in [1, 10, 20, 30]:  # years
+        for rate in [0, 0.04, 0.05]:
+            for sb in [0, 100_000]:  # start balance
+                for mc in [100, 500]:  # regular contributions
+                    for cpy in [2, 4, 12]:  # contributions per year
+                        ci = ut.compound_interest(sb, rate, mc, cpy, y)
+                        cir = ut.compound_interest_rate(sb, ci, mc, cpy, y)
+
+                        assert abs(rate - cir[0]) < TOL
 
 
 def test_monthly_purchasing_power():
@@ -50,17 +69,14 @@ def test_monthly_purchasing_power():
     take into account that we increase the monthly consumption according to the
     inflation rate. The resulting end balance must be zero.
     """
-    for s in range(1, 5):
-        sb = s * 1e6  # start balance
-        for y in range(1, 30):  # years to consume
-            for r in range(0, 10):
-                rate = r / 100  # annual rate of return
-                for i in range(1, 5):
-                    inflation = i / 100  # annual rate of inflation
+    for sb in START_BALANCES:
+        for y in YEARS:  # years to consume
+            for rate in RATES:
+                for inflation in INFLATIONS:
 
-                    mpp = utils.monthly_purchasing_power(sb, y, rate, inflation)
-                    mi = utils.annual_to_monthly(inflation)
-                    mr = utils.annual_to_monthly(rate)
+                    mpp = ut.monthly_purchasing_power(sb, y, rate, inflation)
+                    mi = ut.annual_to_monthly(inflation)
+                    mr = ut.annual_to_monthly(rate)
 
                     # consume the whole start balance
                     balance = sb  # track what remains of the start balance
@@ -81,12 +97,10 @@ def test_value_today():
     """
     Add inflation to today's value to see if that yields the original future value.
     """
-    for i in range(4):
-        x1 = 123 * i  # future value
-        for i in range(1, 5):
-            inflation = i / 100  # annual rate of inflation
-            for years in range(1, 30):
-                y = utils.value_today(x1, years, inflation)  # today's value
-                x2 = y * (1 + inflation) ** years  # add inflation
+    for sb in START_BALANCES:  # future value
+        for inflation in INFLATIONS:
+            for y in YEARS:
+                vt = ut.value_today(sb, y, inflation)  # today's value
+                x = vt * (1 + inflation) ** y  # add inflation
 
-                assert abs(x1 - x2) < TOL
+                assert abs(sb - x) < TOL
